117690dbc67dda3c1ebbf62c6f1f766081e40810
shams <shamsalalfy2005@gmail.com>
2026-03-05 07:52:58 +0200

    feat: add viewProfile, delete user in UserController and add setAvailability and location in DriverController

--- routes/users.js
+++ routes/users.js
@@ -8,47 +8,64 @@ const UserController = require("../controllers/UserController");
 const UserValidator = require("../middlewares/validators/UserValidator");
 
 const uploadAvatar = require("../middlewares/uploadAvatar");
-
+const isAdmin = require("../middlewares/IsAdmin");
 
 // logged user
-router.get("/me",
-    AuthMiddleware,
-    UserController.getProfile
+router.get("/me", AuthMiddleware, UserController.getProfile);
+
+router.patch(
+  "/me",
+  AuthMiddleware,
+  UserValidator.updateProfile(),
+  UserController.updateProfile,
 );
 
-router.patch("/me",
-    AuthMiddleware,
-    UserValidator.updateProfile(),
-    UserController.updateProfile
+router.patch(
+  "/me/password",
+  AuthMiddleware,
+  UserValidator.updatePassword(),
+  UserController.updatePassword,
 );
 
-router.patch("/me/password",
-    AuthMiddleware,
-    UserValidator.updatePassword(),
-    UserController.updatePassword
+router.patch(
+  "/me/avatar",
+  AuthMiddleware,
+  uploadAvatar.single("avatar"),
+  UserValidator.updateAvatar(),
+  UserController.updateAvatar,
 );
 
-router.patch("/me/avatar",
-    AuthMiddleware,
-    uploadAvatar.single("avatar"),
-    UserValidator.updateAvatar(),
-    UserController.updateAvatar
-);
 
+router.get(
+  "/:id",
+  AuthMiddleware,
+  UserValidator.getUserById(),
+  UserController.viewUserProfile,
+);
 
 // admin
-router.get("/",
-    AuthMiddleware,
-    IsAdmin,
-    UserValidator.getUsers(),
-    UserController.getUsers
+router.get(
+  "/",
+  AuthMiddleware,
+  IsAdmin,
+  UserValidator.getUsers(),
+  UserController.getUsers,
 );
 
-router.patch("/:id/role",
-    AuthMiddleware,
-    IsAdmin,
-    UserValidator.updateUserRole(),
-    UserController.updateUserRole
+router.patch(
+  "/:id/role",
+  AuthMiddleware,
+  IsAdmin,
+  UserValidator.updateUserRole(),
+  UserController.updateUserRole,
+);
+
+router.delete(
+  "/:id",
+  AuthMiddleware,
+  IsAdmin,
+  UserValidator.deleteUser(),
+  UserController.deleteUser,
 );
 
 module.exports = router;
